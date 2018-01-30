96f29ed1acad2026cfefd08902b988fe4f5aabe0
MITECH\erica <erica@chameleonpower.com>
2018-01-30 16:31:12 -0500

    App can connect to database with NHibernate

--- SimpleBlog/Areas/Admin/Controllers/UsersController.cs
+++ SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SimpleBlog.Infrastructure;
+using SimpleBlog.Areas.Admin.ViewModels;
+using SimpleBlog.Models;
 
 namespace SimpleBlog.Areas.Admin.Controllers
 {
@@ -13,7 +15,10 @@ namespace SimpleBlog.Areas.Admin.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View(new UsersIndex
+            {
+                Users = Database.Session.Query<User>().ToList()
+            });
         }
     }
 }
